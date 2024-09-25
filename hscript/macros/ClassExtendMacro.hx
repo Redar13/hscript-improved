dd1757469572df9d78969951042e59ba3205f20e
Redar13321 <redar13@mail.ru>
2024-09-26 04:37:48 +0500

    Update ClassExtendMacro.hx

--- hscript/macros/ClassExtendMacro.hx
+++ hscript/macros/ClassExtendMacro.hx
@@ -21,7 +21,7 @@ class ClassExtendMacro {
 	public static inline final FUNC_PREFIX = "_HX_SUPER__";
 	public static inline final CLASS_SUFFIX = "_HSX";
 
-	public static var unallowedMetas:Array<String> = [":hscriptClassPreProcessed", ":structInit", ":bitmap", ":noCustomClass", ":generic"];
+	public static var unallowedMetas:Array<String> = [":hscriptClassPreProcessed", ":structInit", ":bitmap", ":noCustomClass", ":generic", "hscriptClassPreProcessed", ":hscriptClass"];
 
 	public static var modifiedClasses:Array<String> = [];
 
